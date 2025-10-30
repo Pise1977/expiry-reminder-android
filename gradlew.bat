e82e1da9ad7d7f2b34d1cd30658d5d1af3fb1d2e
Pise1977 <marco.daccoai@gmail.com>
2025-10-30 22:27:19 +0100

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-######################################################################
-# Gradle start up script (minimal stub). Ensure executable permission.
-######################################################################
-PRG="$0"
-PRGDIR=`dirname "$PRG"`
-exec "$PRGDIR/gradlew" "$@"
